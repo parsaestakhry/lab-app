3884269591ea9d0ea933484b5a7ae9dc719bf3cf
Parsa Estakhri <parsaestakhry03@gmail.com>
2023-09-14 19:43:25 +0330

    added to repository

--- lab_backend/api/models.py
+++ lab_backend/api/models.py
@@ -4,6 +4,16 @@ from django.db import models
 
 class Item(models.Model):
     item = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now=True)
+
 
     def __str__(self) -> str:
         return self.item
+
+class SubItem (models.Model):
+    sub_item = models.CharField(max_length=128)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.sub_item
+    
